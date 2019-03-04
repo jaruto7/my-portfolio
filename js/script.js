595f63e8ac9cee2edd315f73f8f77bd359365fac
Jarek Kajko <jaruto7@gmail.com>
2019-03-04 20:03:50 +0100

    Remove hobby and modify motto section

--- js/script.js
+++ js/script.js
@@ -1,38 +1,10 @@
-// Stwórz zmienną stałą przechowującą obiekty slidera
-const slideList = [{
-    img: "images/motto.jpg",
-    text: "Sukces jest wtedy gdy okazja spotyka się z przygotowaniem.",
-    author: "Jimmy Johnson"
-  },
-  {
-    img: "images/motto2.jpg",
-    text: "Do sukcesu nie ma windy, trzeba iść po schodach.",
-    author: "Emil Oesch"
-  },
-  {
-    img: "images/motto3.jpg",
-    text: "Tańcz tak, jakby nikt nie patrzył. Śpiewaj tak, jakby nikt nie słuchał. Kochaj to co robisz jakby nikt się nigdy o tym nie dowiedział i żyj tak, jakby to niebo było na ziemi.",
-    author: "Mark Twain"
-  }
-];
+// Interfejs
 
 // Przypisz do zmiennych stałych pobrane elementy portfolio
 const portfolioModal = document.querySelectorAll('.portfolio .project');
 // Pobierz element modala i zapisz w zmiennej stałej
 const modalImage = document.querySelector('.modal-wrap .middle img');
 const modalHref = document.querySelector('.modal-wrap .middle a');
-// Przypisz do zmiennych stałych elementy slidera
-const image = document.querySelector('img.slider');
-const h1 = document.querySelector('p.text');
-const author = document.querySelector('p.author');
-// Zamień węzły listy na obiekt tablicy aby użyć metody finIndex w funkcji changeDot() i pobierz wszystkie kropki znajdujące się w elemencie span
-const dots = [...document.querySelectorAll('.dots span')];
-
-// Interfejs
-// Ustaw czas i zapisz w zmiennej (tysiąc milisekund = 1 sekunda)
-const time = 13000;
-// Ustaw zmienną która będzie pełniła rolę indexu tablicy i przypisz jej wartość poczatkową 
-let activeSlide = 0;
 
 // Implementacja
 
@@ -57,81 +29,37 @@ const closeModal = () => {
   document.querySelector('#wrapper').classList.toggle('blur');
 }
 
-// Stwórz funkcję która przechowuje odwołanie do indexu konkretnego obiektu listy slidera
-const setSlideList = () => {
-  image.src = slideList[activeSlide].img;
-  h1.textContent = slideList[activeSlide].text;
-  author.textContent = slideList[activeSlide].author;
-  // wywołaj funkcję odpowiedzialną za zmianę położenia kropek w sliderze 
-  changeDot();
-  // przypisz aktualny index aby setInterval się włączył ponownie za każdym razem gdy funkcja setSlideList jest wywołana
-  indexInterval = setInterval(changeSlide, time);
-}
-
-// Stwórz funkcję anonimową która odpowiada za zmianę położenia kropek w sliderze 
-const changeDot = () => {
-  // Użyj metody findIndex aby przeszukać elementy które zawierają klasę active i zwróć numer indexu do nowej zmiennej stałej
-  const activeDot = dots.findIndex(dot => dot.classList.contains('active'));
-  // Wyszukaj zwrócony index elementu kropki i zabierz jej klasę 
-  dots[activeDot].classList.remove('active');
-  // Dodaj klasę następnemu elementowi który jej nie ma
-  dots[activeSlide].classList.add('active');
-}
-
-// Stwórz zmienną która będzie przełączać slider po określonym czasie
-const changeSlide = () => {
-  // Inkrementuj index tablicy
-  activeSlide++;
-  // Jeśli index tablicy jest równy długości tablicy to przypisz wartość początkową aby wrócić do 1 elementu tablicy
-  if (activeSlide === slideList.length) {
-    activeSlide = 0;
-  }
-  // Przypisz wartości elementom tablicy obiektu aby podmienić elementy za każdym razem gdy funkcja jest wywoływana
-  image.src = slideList[activeSlide].img;
-  h1.textContent = slideList[activeSlide].text;
-  author.textContent = slideList[activeSlide].author;
-  // Wywołaj funkcję która odpowiada za położenie kropek w sliderze
-  changeDot();
-}
-
-const checkSlider = () => {
-  // Jeśli index tablicy jest równy długości tablicy to przypisz wartość początkową
-  if (activeSlide === slideList.length) {
-    activeSlide = 0;
-    // Jeśli zmienna jest mniejsza niż zero to przypisz jej długość tablicy minus 1 aby równał się indexowi tablicy która przechowuje właściwą ilość obiektów tej tablicy
-  } else if (activeSlide < 0) {
-    activeSlide = slideList.length - 1;
-  }
-  setSlideList();
-}
-
-// Ustaw zmienną i przypisz metodę setInterval która będzie się wykonywać ciągle po określonym czasie
-let indexInterval = setInterval(changeSlide, time);
-
-// Stwórz funkcję anonimową odpowiedzialną za przełączanie strzałki slidera w prawo
-const goForward = () => {
-  // preinkrementuj zmienną odp. za nr. indexu tablicy
-  ++activeSlide;
-  // Wyczyść i zatrzymaj interwał i przekaż index interwału aby nie powodował konfliktu z interwałem przy przełączaniu slidera
-  clearInterval(indexInterval);
-  // Wywołaj funkcję sprawdzającą czy długość tablicy odpowiada długości elementom indexu tablicy
-  checkSlider();
-}
-
-// Stwórz funkcję anonimową odpowiedzialną za przełączanie strzałki slidera w lewo
-const goBack = () => {
-  // console.log(activeSlide);
-  // Predekrementuj zmienną odp. za nr. indexu tablicy
-  --activeSlide;
-  clearInterval(indexInterval);
-  checkSlider();
-}
 
 //Użyj metody forEach aby iterować elementy portfolio i nasłuchuj na zdarzenie kliknięcia przez użytkownika
 portfolioModal.forEach(img => img.addEventListener('click', clickModal));
 // Pobierz element odpowiedzialny za zamykanie modala i nasłuchuj na zdarzenie kliknięcia
 document.querySelector('span.close').addEventListener('click', closeModal);
-// Pobierz obrazki strzałek slidera i nasłuchuj na ich kliknięcie 
-document.querySelector('div.trans-right').addEventListener('click', goForward);
 
-document.querySelector('div.trans-left').addEventListener('click', goBack);
+document.addEventListener('scroll', function () {
+  const slideLeft = document.querySelector('.slide-left');
+  const windowSlideLeftHeight = slideLeft.clientHeight;
+  const scrollSlideLeftValue = window.scrollY;
+  const slideLeftFromTop = slideLeft.offsetTop;
+  const slideLeftHeight = slideLeft.offsetHeight;
+
+  const slideRight = document.querySelector('.slide-right');
+  const windowSlideRightHeight = slideRight.clientHeight;
+  const scrollSlideRightValue = window.scrollY;
+  const slideRightFromTop = slideRight.offsetTop;
+  const slideRightHeight = slideRight.offsetHeight;
+
+  if (scrollSlideLeftValue > slideLeftFromTop + slideLeftHeight - windowSlideLeftHeight * 3) {
+    slideLeft.classList.add('active');
+  }
+
+  if (scrollSlideRightValue > slideRightFromTop + slideRightHeight - windowSlideRightHeight * 2.5) {
+    slideRight.classList.add('active');
+  }
+
+
+
+  if (scrollSlideLeftValue < 100 && scrollSlideRightValue < 100) {
+    slideLeft.classList.remove('active');
+    slideRight.classList.remove('active');
+  }
+})
